f6625d4aab588cf0ad4886e84d33fc6da98fb9c1
awstransform <ecs-user@ip-10-0-3-193.ec2.internal>
2026-01-12 09:59:56 +0000

    AWS Transform commit for jobId 7ca508d1-5601-4824-9046-fa970d45c0df

---
+++ Dominio/CarroDeCompras.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class CarroDeCompras
+    {
+
+        private int numeroCliente;
+        private string productoComprar;
+        private decimal precioFinal;
+
+        public int NumeroCliente
+        {
+            get { return numeroCliente; }
+            set { numeroCliente = value; }
+        }
+
+        public string ProductoComprar
+        {
+            get { return productoComprar; }
+            set { productoComprar = value; }
+        }
+
+        public decimal PrecioFinal
+        {
+            get { return precioFinal; }
+            set { precioFinal = value; }
+        }
+    }
+}
---
+++ Dominio/Usuarios.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuarios
+    {
+        private int id;
+        private string email;
+        private string contraseña;
+        private int tipoDeUsuario;
+        private string fechaNacimiento;
+        private string nombre;
+        private string apellido;
+        private string imagenPerfil;
+
+
+        //metodos
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+
+        }
+
+        public string Contraseña
+        {
+            get { return contraseña; }
+            set { contraseña = value; }
+        }
+
+        public int TipoDeUsuario
+        {
+            get { return tipoDeUsuario; }
+            set { tipoDeUsuario = value; }
+        }
+
+        public string FechaNacimiento
+        {
+            get { return fechaNacimiento; }
+            set { fechaNacimiento = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        public string ImagenDePerfil
+        {
+            get { return imagenPerfil; }
+            set { imagenPerfil = value; }
+        }
+    }
+}
